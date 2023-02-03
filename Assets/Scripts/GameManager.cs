35095b2842d0dcb4cb74a33b79ad3d705cd0890a
cryptocashcashoo <cryptocashcashoo@gmail.com>
2023-02-03 19:20:44 +0100

    First commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+
+    }
+
+    
+    void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
